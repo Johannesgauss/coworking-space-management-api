8fab645b468e162c7f87df7b1577112cc3357fc2
MarcleoSilva <marceloaraujosilva739@gmail.com>
2026-07-07 22:03:06 -0300

    Authorization Strategies, guards, authentication decorator and authorization methods: login and generate tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,16 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { NotificationService } from 'src/common/mail/notification.service';
 import { PrismaModule } from 'src/common/prisma/prisma.module';
+import {JwtModule } from '@nestjs/jwt'
 
 @Module({
-  providers: [AuthService, NotificationService],
-  controllers: [AuthController],
-  imports: [PrismaModule]
-})
+  imports: [PrismaModule,NotificationService, JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: '15m'}
+    })],
+  providers: [AuthService],
+  controllers: [AuthController]})
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,12 +4,14 @@ import { PrismaService } from 'src/common/prisma/prisma.service';
 import argon2 from 'argon2'
 import { nanoid } from "nanoid";
 import { NotificationService } from 'src/common/mail/notification.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly prisma: PrismaService,
-        private readonly mailer: NotificationService
+        private readonly mailer: NotificationService,
+        private readonly jwtService: JwtService
     ) { }
 
     async registerAccount(dto: CreateAccountDto) {
@@ -61,7 +63,6 @@ export class AuthService {
         if (!userEmailRegistration || userEmailRegistration.expiresAt < new Date()) return { message: 'Inválido ou Token expirado' };
 
 
-
         return await this.prisma.$transaction(async (tx) => {
             await tx.user.update({
                 where: {
@@ -80,6 +81,56 @@ export class AuthService {
 
             return {message: "Conta confirmada com sucesso! Siga para o Login"}
         })
+    }
+
+    async generateTokens(userId: string) {
+        const refreshTokenId = nanoid();
+
+        const [accessToken, refreshToken] = await Promise.all([
+            this.jwtService.signAsync({
+                iss: process.env.JWT_ISSUER,
+                sub: userId
+            }, {expiresIn: '15m'}),
+            this.jwtService.signAsync(
+                {
+                    iss: process.env.JWT_ISSUER,
+                    jti: refreshTokenId,
+                    sub: userId
+                }, {expiresIn: '7d'}
+            )
+        ])
+
+        const hashedRefreshToken = await argon2.hash(refreshToken);
+
+        await this.prisma.refreshToken.create({
+            data: {
+                userId: userId,
+                id: refreshTokenId,
+                token: hashedRefreshToken,
+                expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+            }
+        })
+
+        return {accessToken, refreshToken}
+    }
+
+    async login(email: string, password: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {email: email}
+        })
+
+        if (!user) return {message: 'Credenciais inválidas'};
+
+        const validatePassword = await argon2.verify(
+            user.password,
+            password
+        )
+
+        if (!validatePassword) return {message: "Credenciais inválidas"};
+
+        const {accessToken, refreshToken} = await this.generateTokens(user.id)
+
+        return {accessToken: accessToken, refreshToken: refreshToken}
     }
 }
 
