3cd0fc97b06f61c1013e377c7d4077c36ccb7b79
MarcleoSilva <marceloaraujosilva739@gmail.com>
2026-07-09 14:56:18 -0300

    update user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Patch, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/common/decorators/user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +11,11 @@ export class UserController {
     @HttpCode(HttpStatus.OK)
     getMe(@User('id') userId: string) {
         return this.userService.getMe(userId);
+    }
+
+    @Put('me')
+    @HttpCode(HttpStatus.OK)
+    updatePersonalUserData(@User('id') userId: string, dto: UpdateUserDto) {
+        return this.userService.updateUserData(userId, dto)
     }
 }
